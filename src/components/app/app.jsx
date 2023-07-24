626c3bb28e663f42fdc072950b977fa322b7a2bd
Katarios2015 <prichal7@bk.ru>
2023-07-24 10:39:38 +0300

    добавляет первые реакт компоненты,устанавливает необходимые зависимости

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import MainPage from "../main-page/main-page";
+
+const App = (props) => {
+  const {cardsCount} = props;
+  return (
+    <MainPage cardsCount={cardsCount}/>
+  );
+};
+
+App.propTypes = {
+  cardsCount: PropTypes.number.isRequired,
+};
+
+export default App;
