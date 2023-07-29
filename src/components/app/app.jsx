4953fc60636de408929e30b4f9db25e8bd3fd9cf
Katarios2015 <prichal7@bk.ru>
2023-07-29 10:37:43 +0300

    Добавляет новые компоненты и маршруты к ним

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,36 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import MainPage from "../main-page/main-page";
+import Favorities from "../favorites/favorites";
+import Login from "../login/login";
+import NotFound from "../not-found/not-found";
+import Property from "../property/property";
+
 
 const App = (props) => {
   const {cardsCount} = props;
   return (
-    <MainPage cardsCount={cardsCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <MainPage cardsCount={cardsCount}/>
+        </Route>
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+        <Route exact path="/favorites">
+          <Favorities/>
+        </Route>
+        <Route exact path="/offer/:id">
+          <Property/>
+        </Route>
+        <Route>
+          <NotFound/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 };
 
